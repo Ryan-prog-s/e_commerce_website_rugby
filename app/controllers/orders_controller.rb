25815dc4890b93f088cf881b625be7b84c492cdd
Ryan-prog-s <ryan.guillerm@gmail.com>
2024-06-28 15:40:23 +0200

    Version finale

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,24 +1,32 @@
 class OrdersController < ApplicationController
-    before_action :require_buyer
-    before_action :require_login
+  before_action :require_buyer
+  before_action :require_login
   
-    def create
-      @order = current_user.buyer.orders.new(order_params)
-      @order.product = Product.find(params[:product_id])
-      if @order.save
-        redirect_to @order.product, notice: 'La commande a été créée avec succès.'
-      else
-        redirect_to @order.product, alert: 'La commande n\'a pas pu être créée.'
+  def create
+    product = Product.find(order_params[:product_id])
+    @order = current_user.buyer.orders.new(order_params)
+    @order.product = product
+
+    if product.stock >= @order.quantity
+      ActiveRecord::Base.transaction do
+        @order.save!
+        product.update!(stock: product.stock - @order.quantity)
       end
+      redirect_to @order.product, notice: 'La commande a été créée avec succès.'
+    else
+      redirect_to @order.product, alert: 'Quantité demandée non disponible en stock.'
     end
+  rescue ActiveRecord::RecordInvalid
+    redirect_to @order.product, alert: 'La commande n\'a pas pu être créée.'
+  end
   
-    private
-  
-    def order_params
-      params.require(:order).permit(:quantity)
-    end
-  
-    def require_buyer
-      redirect_to root_path, alert: 'Seuls les acheteurs peuvent effectuer cette action' unless current_user&.buyer
-    end
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity, :product_id)
+  end
+
+  def require_buyer
+    redirect_to root_path, alert: 'Seuls les acheteurs peuvent effectuer cette action' unless current_user&.buyer
+  end
 end
